0692082a75db47ec1a579d0e5ee361fbfe6d4b06
hafiyyannn <naufalhafiyyan10@gmail.com>
2022-08-03 16:26:36 +0700

    create migration and model for chapter component

---
+++ database/migrations/2022_08_03_091405_create_chapter_videos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChapterVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chapter_videos', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('filename');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chapter_videos');
+    }
+}
